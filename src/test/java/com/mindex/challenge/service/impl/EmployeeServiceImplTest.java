b653db5ba1675e521f6985be7564e00ec0ca9368
Justin Davis <justintdavis8@gmail.com>
2023-04-05 14:23:18 -0500

    Squashed commit of the following:
    commit 2823341a956437e7e5fee39e13f6b1738ab81e42
    Author: Justin Davis <justintdavis8@gmail.com>
    Date:   Wed Apr 5 14:22:50 2023 -0500
    
        Added tests for total number of reports and employee compensation

--- src/test/java/com/mindex/challenge/service/impl/EmployeeServiceImplTest.java
+++ src/test/java/com/mindex/challenge/service/impl/EmployeeServiceImplTest.java
@@ -1,6 +1,8 @@
 package com.mindex.challenge.service.impl;
 
+import com.mindex.challenge.data.Compensation;
 import com.mindex.challenge.data.Employee;
+import com.mindex.challenge.data.ReportingStructure;
 import com.mindex.challenge.service.EmployeeService;
 import org.junit.Before;
 import org.junit.Test;
@@ -15,8 +17,11 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.UUID;
+
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -24,6 +29,10 @@ public class EmployeeServiceImplTest {
 
     private String employeeUrl;
     private String employeeIdUrl;
+    private String reportingStructureUrl;
+    private String compensationUrl;
+    private String compensationIdUrl;
+    private String employeeId;
 
     @Autowired
     private EmployeeService employeeService;
@@ -38,6 +47,10 @@ public class EmployeeServiceImplTest {
     public void setup() {
         employeeUrl = "http://localhost:" + port + "/employee";
         employeeIdUrl = "http://localhost:" + port + "/employee/{id}";
+        reportingStructureUrl = "http://localhost:" + port + "/get-reporting-structure/{id}";
+        compensationUrl = "http://localhost:" + port + "/create-compensation";
+        compensationIdUrl = "http://localhost:" + port + "/get-compensation/{id}";
+        employeeId = "16a596ae-edd3-4847-99fe-c4518e82c86f";
     }
 
     @Test
@@ -75,6 +88,54 @@ public class EmployeeServiceImplTest {
                         readEmployee.getEmployeeId()).getBody();
 
         assertEmployeeEquivalence(readEmployee, updatedEmployee);
+    }
+
+    @Test
+    public void getTotalReportingStructureTest() {
+        Employee testEmployee = new Employee();
+
+        testEmployee.setFirstName("John");
+        testEmployee.setLastName("Doe");
+        testEmployee.setDepartment("Engineering");
+        testEmployee.setPosition("Developer");
+
+        ReportingStructure expectedReportingStructure = new ReportingStructure();
+
+        expectedReportingStructure.setEmployee(testEmployee);
+        expectedReportingStructure.setNumberOfReports(4);
+
+        ReportingStructure actualEmployeeReportingStructure = restTemplate.getForEntity(reportingStructureUrl, ReportingStructure.class, employeeId).getBody();
+
+        assertEquals(expectedReportingStructure.getNumberOfReports(), actualEmployeeReportingStructure.getNumberOfReports());
+    }
+
+    @Test
+    public void createReadEmployeeCompensationTest() {
+        Employee testEmployee = new Employee();
+
+        testEmployee.setEmployeeId(UUID.randomUUID().toString());
+        testEmployee.setFirstName("John");
+        testEmployee.setLastName("Doe");
+        testEmployee.setDepartment("Engineering");
+        testEmployee.setPosition("Developer");
+
+        Compensation testCompensation = new Compensation();
+
+        testCompensation.setCompensationId(UUID.randomUUID().toString());
+        testCompensation.setEmployee(testEmployee);
+        testCompensation.setSalary(100000);
+        testCompensation.setEffectiveDate(Instant.now());
+
+        Compensation createdEmployeeCompensation = restTemplate.postForEntity(compensationUrl, testCompensation, Compensation.class).getBody();
+        Compensation getEmployeeCompensation = restTemplate.getForEntity(compensationIdUrl, Compensation.class, createdEmployeeCompensation.getEmployee().getEmployeeId()).getBody();
+
+        assertNotNull(createdEmployeeCompensation.getEmployee().getEmployeeId());
+        assertEquals(createdEmployeeCompensation.getEmployee().getFirstName(), getEmployeeCompensation.getEmployee().getFirstName());
+        assertEquals(createdEmployeeCompensation.getEmployee().getLastName(), getEmployeeCompensation.getEmployee().getLastName());
+        assertEquals(createdEmployeeCompensation.getEmployee().getDepartment(), getEmployeeCompensation.getEmployee().getDepartment());
+        assertEquals(createdEmployeeCompensation.getEmployee().getPosition(), getEmployeeCompensation.getEmployee().getPosition());
+        assertEquals(createdEmployeeCompensation.getSalary(), getEmployeeCompensation.getSalary());
+        assertEquals(createdEmployeeCompensation.getEffectiveDate().truncatedTo(ChronoUnit.MINUTES), getEmployeeCompensation.getEffectiveDate().truncatedTo(ChronoUnit.MINUTES));
     }
 
     private static void assertEmployeeEquivalence(Employee expected, Employee actual) {
