c14d01b24a1c72a77e14d8de7b4e9db57f77fe4d
Justin Davis <justintdavis8@gmail.com>
2023-04-05 10:07:35 -0500

    Implemented employee compensation endpoint

---
+++ src/main/java/com/mindex/challenge/service/impl/CompensationServiceImp.java
@@ -0,0 +1,52 @@
+package com.mindex.challenge.service.impl;
+
+import com.mindex.challenge.dao.CompensationRepository;
+import com.mindex.challenge.data.Compensation;
+import com.mindex.challenge.request.CompensationRequest;
+import com.mindex.challenge.service.CompensationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Service
+public class CompensationServiceImp implements CompensationService {
+    private static final Logger LOG = LoggerFactory.getLogger(CompensationServiceImp.class);
+
+    @Autowired
+    private CompensationRepository compensationRepository;
+
+    @Override
+    public Compensation getCompensation(String id) {
+        LOG.debug("Retrieving employee compensation with employee id [{}]", id);
+
+        Compensation compensation = compensationRepository.findCompensationByEmployeeEmployeeId(id);
+
+        LOG.info("Employee compensation retrieved");
+
+        return compensation;
+    }
+
+    @Override
+    public Compensation createCompensation(CompensationRequest compensationRequest) {
+        Compensation compensation = new Compensation();
+
+        compensationRequest.getEmployee().setEmployeeId(UUID.randomUUID().toString());
+
+        compensation.setCompensationId(UUID.randomUUID().toString());
+        compensation.setEmployee(compensationRequest.getEmployee());
+        compensation.setEffectiveDate(Instant.now());
+        compensation.setSalary(compensationRequest.getSalary());
+
+        LOG.debug("Creating compensation for employee with id [{}]", compensation.getEmployee().getEmployeeId());
+
+        compensationRepository.insert(compensation);
+
+        LOG.info("Employee compensation created");
+
+        return compensation;
+    }
+}
